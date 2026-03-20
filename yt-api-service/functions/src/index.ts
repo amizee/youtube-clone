6004648cc06e3ca5540b66cc7e49fbe2c66a9592
amizee <kevinkong39@gmail.com>
2026-03-20 23:44:15 +1100

    create user firebase function

---
+++ yt-api-service/functions/src/index.ts
@@ -0,0 +1,21 @@
+import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions/v1";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+
+initializeApp();
+
+const firestore = new Firestore();
+
+export const createUser = functions.auth.user().onCreate(async (user) => {
+  logger.info(`${JSON.stringify(user)}`);
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
+
+  await firestore.collection("users").doc(user.uid).set(userInfo);
+  logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+  return;
+});
