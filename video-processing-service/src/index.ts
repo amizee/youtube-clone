30ad646f03fdbb47710a10f84774cd61060c4e29
amizee <kevinkong39@gmail.com>
2026-03-24 21:37:04 +1100

    add video metadata to firestore + pub/sub retry bug

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -8,7 +8,7 @@ import {
   convertVideo,
   setupDirectories
 } from './storage';
-import { readFile } from 'fs';
+import { isVideoNew, setVideo } from "./firestore";
 
 // Create the local directories for videos
 setupDirectories();
@@ -32,8 +32,19 @@ app.post('/process-video', async (req, res) => {
     return res.status(400).send('Bad Request: missing filename.');
   }
 
-  const inputFileName = data.name;
+  const inputFileName = data.name; // In format of <UID>-<DATE>.<EXTENSION>
   const outputFileName = `processed-${inputFileName}`;
+  const videoId = inputFileName.split('.')[0];
+
+  if (!isVideoNew(videoId)) {
+    return res.status(400).send('Bad Request: video already processing or processed.');
+  } else {
+    setVideo(videoId, {
+      id: videoId,
+      uid: videoId.split('-')[0],
+      status: 'processing'
+    });
+  }
 
   // Download the raw video from Cloud Storage
   await downloadRawVideo(inputFileName);
@@ -51,6 +62,11 @@ app.post('/process-video', async (req, res) => {
   
   // Upload the processed video to Cloud Storage
   await uploadProcessedVideo(outputFileName);
+
+  setVideo(videoId, {
+    status: 'processed',
+    filename: outputFileName
+  });
 
   await Promise.all([
     deleteRawVideo(inputFileName),
